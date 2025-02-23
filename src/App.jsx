889300fe713c196cbf8ae67ad3824489cafde63b
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-23 13:29:11 +0100

    temporarily test the form in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import MainPage from './components/layout/MainPage';
 import AuthSection from "./components/auth/AuthSection";
+import JobForm from './components/jobManagement/JobForm';
 import API from './api/axios';
 import './App.css'
 
 function App() {
-
+  const [jobToEdit, setJobToEdit] = useState(null);
   return (
     <>
       <Header />
       <main>
         <UserProvider>
           <AuthSection /> 
+          <JobForm />
+          {jobToEdit && <JobForm jobToEdit={jobToEdit} />}
         </UserProvider>
         <MainPage /> 
       </main>
